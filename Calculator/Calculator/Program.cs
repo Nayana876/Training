460e4b02274e1e27a47ded8edb64deae43e20f1a
nrajith <nrajith@IND387>
2021-09-10 03:13:05 -0500

    cal

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sum();
+        }
+
+        private static void sum()
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+}
