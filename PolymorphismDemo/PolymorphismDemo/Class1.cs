572126a02b8146ba8384d31b5e4010c92d1650f9
nrajith <nrajith@IND387>
2021-09-13 06:04:39 -0500

    Polymorphism

--- PolymorphismDemo/PolymorphismDemo/Class1.cs
+++ PolymorphismDemo/PolymorphismDemo/Class1.cs
@@ -11,16 +11,38 @@ namespace PolymorphismDemo
         static int a1 = 10;
         public static void Main(string[] values)
         {
-            int result;
-            Add(out result);
-            Console.Write(result);
+            OutDemo();
+            RefDemo();
             Console.Read();
+
+        }
+        private static void RefDemo()
+        {
+            string str1 = "Ram";//for ref initialization is required
+            setValue(ref str1);
+            Console.WriteLine(str1);
+
         }
 
-        public static bool Add(out int a)
+        static void setValue(ref string str)
+        {
+            str = "qwerty";
+        }
+
+        private static void OutDemo()
+        {
+            int result;
+            float resFloat;
+            Add(out result, out resFloat);
+            Console.WriteLine(result + " " + resFloat);
+        }
+        //out- the out variables must be initialised within the function -initialization before fun call not required
+        //ref -initialisation required before fnction call
+        public static bool Add(out int a,out float b)
         {
             a = 20;
             a = a + a1;
+            b = 10;
             return true;
         }
     }
---
+++ PolymorphismDemo/PolymorphismDemo/OperaterOverload.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismDemo
+{
+    class Calculator
+    {
+        public int num1, num2;
+
+        public Calculator(int v1, int v2)
+        {
+            num1 = v1;
+            num2 = v2;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(num1.ToString()+", "+ num2.ToString());
+        }
+        // className operator operator_symbol(parameter){}
+        public static Calculator operator -(Calculator c1) {
+            c1.num1 = -c1.num1;
+            c1.num2 = -c1.num2;
+            return c1;
+        }
+
+    }
+    class OperatorOverload
+    {
+        public static void Main(string[] args)
+        {
+            Calculator calc = new Calculator(15, -25);
+            calc = -calc;
+            //Console.WriteLine("Num1 = "+calc.num1);
+            calc.print();
+            Console.Read();
+        }
+
+    }
+    
+}
