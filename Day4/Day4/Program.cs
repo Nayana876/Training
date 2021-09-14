48bfd862f6cc01d0ceaca4847c1cd5abcb17a693
nrajith <nrajith@IND387>
2021-09-14 05:18:41 -0500

    IndexedClass

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndexedClass nameList = new IndexedClass();
+            //syntatic sugar
+            nameList[0] = "Ram";
+            Console.WriteLine(nameList[0]);
+            Console.Read();
+        }
+    }
+    class IndexedClass
+    {
+        private string[] names = new string[10];
+        public string this[int index]
+        {
+            get
+            {
+                return (index >= 0 && index <= 9) ? names[index] : "";
+            }
+            set
+            {
+                if (index >= 0 && index <= 9)
+                {
+                    names[index] = value;
+                }
+            }
+        }
+    }
+}
